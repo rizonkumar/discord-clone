ede55550d11e7dc2a74891168d14b952aa851f9c
rizonkumar <rizon.kumar.rahi@gmail.com>
2023-12-19 23:17:33 +0530

    Enivorment setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <p className="text-3xl font-bold">Hello Discord Clone</p>;
-  <p>Check the branch DEC_20-23</p>;
+  return (
+    <div className="flex flex-col">
+      <p className="text-3xl font-bold">
+        Hello Discord Clone Check the branch DEC_20-23
+      </p>
+      <Button>Click Me</Button>
+    </div>
+  );
 }
