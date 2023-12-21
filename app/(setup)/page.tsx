696755344971e6c1c58677c5345149b2f8f8baa1
rizonkumar <rizon.kumar.rahi@gmail.com>
2023-12-21 08:42:11 +0530

    Prisma Setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,24 @@
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { initalProfile } from "@/lib/inital-profile";
+
+const SetupPage = async () => {
+  const profile = await initalProfile();
+
+  const server = await db.server.findFirst({
+    where: {
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <div>Create a Server</div>;
+};
+
+export default SetupPage;
