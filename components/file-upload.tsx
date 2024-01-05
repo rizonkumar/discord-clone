30b933242b407423554ee260324ad5781152e954
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-01-05 11:56:15 +0530

    Upload thing setup

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {X} from "lucide-react";
+import { X } from "lucide-react";
 import Image from "next/image";
 import { UploadDropzone } from "@/lib/uploadthing";
 
@@ -15,24 +15,21 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-
   const fileType = value?.split(".").pop();
-  if(value && fileType !== "pdf") {
+  if (value && fileType !== "pdf") {
     return (
       <div className="relative h-20 w-20">
-        <Image 
-        fill
-        src={value}
-        alt="Upload"
-        className="rounded-full"/>
-        <button onClick={() => onChange("")}
-        className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0
         shadow-sm"
-        type="button">
-          <X className="h-4 w-4"/>
+          type="button"
+        >
+          <X className="h-4 w-4" />
         </button>
       </div>
-    )
+    );
   }
   return (
     <div>
